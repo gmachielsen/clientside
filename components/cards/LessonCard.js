2f3e77164974b08f4e94871ae0888b48f94ac0a5
Gijs Machielsen <gijsmachielsen@iMac-van-Gijs.local>
2021-10-06 15:34:42 +0200

    initial commit

--- components/cards/LessonCard.js
+++ components/cards/LessonCard.js
@@ -5,7 +5,7 @@ const LessonCard = ({ lesson, setPreview, showModal, setShowModal, }) => {
         <div className="lesson" style={{ textAlign: 'center', padding: 'inherit' }}>
                        
 
-                    <Card bordered={false} style={{ position: 'relative', width: 500, height: 500 }} cover={<img style={{ width: 500, height: 500, objectFit: 'cover', textAlign: 'center'}} alt="example" src={lesson.lessonimage && lesson.lessonimage !== null && (lesson.lessonimage.Location)} />}>
+                    <Card bordered={false} style={{ position: 'relative', width: 500, height: 500, marginTop: "100px" }} cover={<img style={{ width: 500, height: 500, objectFit: 'cover', textAlign: 'center'}} alt="example" src={lesson.lessonimage && lesson.lessonimage !== null && (lesson.lessonimage.Location)} />}>
                         
                         <h1 style={{ textAlign: 'center' }}>{lesson.title}</h1>
 
