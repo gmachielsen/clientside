adb9883e06dd4f91fc55d1144431c219f36fa9ed
Gijs Machielsen <gijsmachielsen@iMac-van-Gijs.local>
2021-10-13 16:33:52 +0200

    initial commit

--- components/cards/Cover.js
+++ components/cards/Cover.js
@@ -7,6 +7,8 @@ const Cover = () => {
         <>
             {/* <TopNav /> */}
             <img src={require('../../public/coverphoto.jpg')} style={{ filter: 'brightness(50%) grayscale(50%)', objectFit: 'cover', minWidth: '100%', maxWidth: '100%', maxHeight: '100vh', minHeight: '100vh', display: 'block', zIndex: 0 }} />
+            <img/>
+
         </>
     );
 };
